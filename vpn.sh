08df9fca536fe0d773c8d35d2121aa2d09a49971
Felytweb <69897806+Felytweb@users.noreply.github.com>
2021-01-04 20:41:06 +0500

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -847,7 +847,7 @@ Installation_dependency(){
 }
 Install_SSR(){
 	check_root
-        apt-get update -y && apt install git -y && apt install curl -y && apt install net-tools -y && apt install iptables -y
+        apt-get update -y && apt install git -y && apt install curl -y && apt install net-tools -y && apt install iptables -y && apt install sudo -y
 	[[ -e ${ssr_folder} ]] && echo -e "${Error} ShadowsocksR уже установлен !" && exit 1
 	echo -e "${Info} типа че то происходит..."
 	Set_user_api_server_pub_addr
@@ -1511,11 +1511,11 @@ case "$num" in
 	Download_DB
 	;;
 	8)
-	Set_user_api_server_pub_addr "Modify"
-	Modify_user_api_server_pub_addr
+	Manually_Modify_Config
 	;;
 	9)
-	Manually_Modify_Config
+	Set_user_api_server_pub_addr "Modify"
+	Modify_user_api_server_pub_addr
 	;;
 	10)
 	Clear_transfer
